496f1667192bcc60017c0acf28ea6698cb4a75aa
65011211019 <138645306+65011211019@users.noreply.github.com>
2025-01-12 01:51:22 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-bg': '#100617', // Dark background
+      }
+    },
+  },
+  plugins: [],
+}
